401e2f781d8692c8c7726daab362455ce3173a60
AntiTenzor <anti_tenzor@mail.ru>
2024-01-19 15:47:33 +0300

    Implementation of JsonCommentConverter

---
+++ Src/Newtonsoft.Json/Converters/JsonCommentConverter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Newtonsoft.Json.Converters
+{
+    /// <summary>
+    /// Special converter to add some user comment into generated JSON
+    /// </summary>
+    public class JsonCommentConverter : JsonConverter
+    {
+        private readonly string _comment;
+
+        /// <summary>
+        /// Initialize converter instance with user-defined comment
+        /// </summary>
+        public JsonCommentConverter(string comment)
+        {
+            _comment = comment;
+        }
+
+        /// <inheritdoc/>
+        public override bool CanRead => false;
+
+        /// <inheritdoc/>
+        public override bool CanConvert(Type objectType) => true;
+
+        /// <inheritdoc/>
+        public override void WriteJson(Newtonsoft.Json.JsonWriter writer, object value, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            writer.WriteValue(value);
+
+            if (_comment == null)
+                return;
+
+            int len = _comment.Length;
+            bool hasNormalChar = false;
+            for (int j = 0; j < len; j++)
+            {
+                if (Char.IsWhiteSpace(_comment[j]))
+                    continue;
+
+                hasNormalChar = true;
+                break;
+            }
+
+            if (hasNormalChar)
+            {
+                writer.WriteComment(_comment); // append comment
+            }
+        }
+
+        /// <inheritdoc/>
+        public override object ReadJson(Newtonsoft.Json.JsonReader reader, Type objectType, object existingValue,
+            Newtonsoft.Json.JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
